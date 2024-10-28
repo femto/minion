24bb14154770f49bd5d141b6502dbae0dbcb3e76
femto <femtowin@gmail.com>
2024-10-28 11:32:20 +0800

    lmp_action_node

---
+++ minion/actions/lmp_action_node.py
@@ -0,0 +1,38 @@
+from typing import Any, Union, List
+
+import ell
+
+from minion.configs.config import config
+from minion.message_types import Message
+from minion.actions.action_node import LLMActionNode
+from minion.providers import create_llm_provider
+
+class LmpActionNode(LLMActionNode):
+    def __init__(self, llm, output_parser=None):
+        super().__init__(llm, output_parser)
+        ell.init(**config.ell, default_client=self.llm.client0)
+
+    @ell.complex(model="gpt-4o-mini")
+    def ell_call(self, ret):
+        """You are a helpful assistant."""
+        return ret
+
+    async def execute(self, messages: Union[str, Message, List[Message]], **kwargs) -> Any:
+        # 从 llm.config 获取配置
+        api_params = {
+            # "api_type": self.llm.config.api_type,
+            # "api_key": self.llm.config.api_key,
+            # "base_url": self.llm.config.base_url,
+            "temperature": self.llm.config.temperature,
+        }
+        
+        # 将 kwargs 合并到 api_params 中，允许覆盖默认值
+        api_params.update(kwargs)
+        
+        response = self.ell_call(messages, client=self.llm.client0, api_params=api_params)
+        response = response.text
+
+        if self.output_parser:
+            return self.output_parser(response)
+
+        return response
